f965e329adc3144f4ff3b5ae5f073a7a1f323698
Thibault Deckers <thibault.deckers@gmail.com>
2019-12-23 20:15:38 +0900

    fixed search result page

--- lib/widgets/album/search_delegate.dart
+++ lib/widgets/album/search_delegate.dart
@@ -1,6 +1,7 @@
 import 'package:aves/model/image_collection.dart';
 import 'package:aves/model/image_entry.dart';
 import 'package:aves/widgets/album/thumbnail_collection.dart';
+import 'package:aves/widgets/common/media_query_data_provider.dart';
 import 'package:flutter/material.dart';
 
 class ImageSearchDelegate extends SearchDelegate<ImageEntry> {
@@ -54,18 +55,39 @@ class ImageSearchDelegate extends SearchDelegate<ImageEntry> {
     final lowerQuery = query.toLowerCase();
     final matches = collection.sortedEntries.where((entry) => entry.search(lowerQuery)).toList();
     if (matches.isEmpty) {
-      return Center(
-        child: Text(
-          'No match',
-          textAlign: TextAlign.center,
-        ),
-      );
+      return _EmptyContent();
     }
-    return ThumbnailCollection(
-      collection: ImageCollection(
-        entries: matches,
-        groupFactor: collection.groupFactor,
-        sortFactor: collection.sortFactor,
+    return MediaQueryDataProvider(
+      child: ThumbnailCollection(
+        collection: ImageCollection(
+          entries: matches,
+          groupFactor: collection.groupFactor,
+          sortFactor: collection.sortFactor,
+        ),
+      ),
+    );
+  }
+}
+
+class _EmptyContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const color = Color(0xFF607D8B);
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          Icon(
+            Icons.photo,
+            size: 64,
+            color: color,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'No match',
+            style: TextStyle(color: color),
+          ),
+        ],
       ),
     );
   }
