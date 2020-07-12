2a57144dc66672a1a502bf6ff88c3cf168f1d8f2
Thibault Deckers <thibault.deckers@gmail.com>
2020-07-12 14:11:54 +0900

    CI: release on tag with Github Actions

---
+++ lib/flutter_version.dart
@@ -0,0 +1,10 @@
+// run `scripts/update_flutter_version.sh` to update with the content of `flutter --version --machine`
+const Map<String, String> version = {
+  'channel': 'unknown',
+  'dartSdkVersion': 'unknown',
+  'engineRevision': 'unknown',
+  'frameworkCommitDate': 'unknown',
+  'frameworkRevision': 'unknown',
+  'frameworkVersion': 'unknown',
+  'repositoryUrl': 'unknown',
+};
