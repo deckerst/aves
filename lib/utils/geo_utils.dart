cb8b3f598eaaa6fcdf9016447fc2edf50e8bbc5d
Thibault Deckers <thibault.deckers@42dot.ai>
2020-08-21 11:00:21 +0900

    location: fixed conversion from decimal degree to DMS

--- lib/utils/geo_utils.dart
+++ lib/utils/geo_utils.dart
@@ -3,8 +3,7 @@ import 'dart:math' as math;
 import 'package:intl/intl.dart';
 import 'package:tuple/tuple.dart';
 
-// adapted from Mike Mitterer's dart-latlong library
-String _decimal2sexagesimal(final double dec) {
+String _decimal2sexagesimal(final double degDecimal) {
   double _round(final double value, {final int decimals = 6}) => (value * math.pow(10, decimals)).round() / math.pow(10, decimals);
 
   List<int> _split(final double value) {
@@ -17,22 +16,15 @@ String _decimal2sexagesimal(final double dec) {
     ];
   }
 
-  final parts = _split(dec);
-  final integerPart = parts[0];
-  final fractionalPart = parts[1];
+  final deg = _split(degDecimal)[0];
+  final minDecimal = (degDecimal.abs() - deg) * 60;
+  final min = _split(minDecimal)[0];
+  final sec = (minDecimal - min) * 60;
 
-  final deg = integerPart;
-  final min = double.parse('0.$fractionalPart') * 60;
-
-  final minParts = _split(min);
-  final minFractionalPart = minParts[1];
-
-  final sec = double.parse('0.$minFractionalPart') * 60;
-
-  return '$deg° ${min.floor()}′ ${_round(sec, decimals: 2).toStringAsFixed(2)}″';
+  return '$deg° $min′ ${_round(sec, decimals: 2).toStringAsFixed(2)}″';
 }
 
-// return coordinates formatted as DMS, e.g. ['41°24′12.2″ N', '2°10′26.5″E']
+// return coordinates formatted as DMS, e.g. ['41° 24′ 12.2″ N', '2° 10′ 26.5″ E']
 List<String> toDMS(Tuple2<double, double> latLng) {
   if (latLng == null) return [];
   final lat = latLng.item1;
