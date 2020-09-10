7e530aed748e32c2d114809bee8610fa57f0ea8f
Thibault Deckers <thibault.deckers@gmail.com>
2020-09-10 14:59:50 +0900

    sort chips on countries/tags pages

--- lib/widgets/filter_grids/albums_page.dart
+++ lib/widgets/filter_grids/albums_page.dart
@@ -5,15 +5,11 @@ import 'package:aves/model/source/album.dart';
 import 'package:aves/model/source/collection_source.dart';
 import 'package:aves/model/source/enums.dart';
 import 'package:aves/utils/android_file_utils.dart';
-import 'package:aves/utils/durations.dart';
 import 'package:aves/widgets/collection/empty.dart';
 import 'package:aves/widgets/common/aves_selection_dialog.dart';
 import 'package:aves/widgets/common/icons.dart';
-import 'package:aves/widgets/common/menu_row.dart';
 import 'package:aves/widgets/filter_grids/filter_grid_page.dart';
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 
 class AlbumListPage extends StatelessWidget {
@@ -27,51 +23,29 @@ class AlbumListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Selector<Settings, ChipSortFactor>(
       selector: (context, s) => s.albumSortFactor,
-      builder: (context, albumSortFactor, child) {
+      builder: (context, sortFactor, child) {
         return AnimatedBuilder(
           animation: androidFileUtils.appNameChangeNotifier,
           builder: (context, child) => StreamBuilder(
             stream: source.eventBus.on<AlbumsChangedEvent>(),
-            builder: (context, snapshot) {
-              return FilterNavigationPage(
-                source: source,
-                title: 'Albums',
-                actions: _buildActions(context),
-                filterEntries: getAlbumEntries(source),
-                filterBuilder: (s) => AlbumFilter(s, source.getUniqueAlbumName(s)),
-                emptyBuilder: () => EmptyContent(
-                  icon: AIcons.album,
-                  text: 'No albums',
-                ),
-              );
-            },
+            builder: (context, snapshot) => FilterNavigationPage(
+              source: source,
+              title: 'Albums',
+              onChipActionSelected: _onChipActionSelected,
+              filterEntries: getAlbumEntries(source),
+              filterBuilder: (s) => AlbumFilter(s, source.getUniqueAlbumName(s)),
+              emptyBuilder: () => EmptyContent(
+                icon: AIcons.album,
+                text: 'No albums',
+              ),
+            ),
           ),
         );
       },
     );
   }
 
-  List<Widget> _buildActions(BuildContext context) {
-    return [
-      PopupMenuButton<ChipAction>(
-        key: Key('appbar-menu-button'),
-        itemBuilder: (context) {
-          return [
-            PopupMenuItem(
-              key: Key('menu-sort'),
-              value: ChipAction.sort,
-              child: MenuRow(text: 'Sort...', icon: AIcons.sort),
-            ),
-          ];
-        },
-        onSelected: (action) => _onChipActionSelected(context, action),
-      ),
-    ];
-  }
-
   void _onChipActionSelected(BuildContext context, ChipAction action) async {
-    // wait for the popup menu to hide before proceeding with the action
-    await Future.delayed(Durations.popupMenuAnimation * timeDilation);
     switch (action) {
       case ChipAction.sort:
         final factor = await showDialog<ChipSortFactor>(
@@ -96,20 +70,17 @@ class AlbumListPage extends StatelessWidget {
 
   static Map<String, ImageEntry> getAlbumEntries(CollectionSource source) {
     final entriesByDate = source.sortedEntriesForFilterList;
-    final albumEntries = source.sortedAlbums.map((album) {
-      return MapEntry(
-        album,
-        entriesByDate.firstWhere((entry) => entry.directory == album, orElse: () => null),
-      );
-    }).toList();
+    final albums = source.sortedAlbums
+        .map((album) => MapEntry(
+              album,
+              entriesByDate.firstWhere((entry) => entry.directory == album, orElse: () => null),
+            ))
+        .toList();
 
     switch (settings.albumSortFactor) {
       case ChipSortFactor.date:
-        albumEntries.sort((a, b) {
-          final c = b.value.bestDate?.compareTo(a.value.bestDate) ?? -1;
-          return c != 0 ? c : compareAsciiUpperCase(a.key, b.key);
-        });
-        return Map.fromEntries(albumEntries);
+        albums.sort(FilterNavigationPage.compareChipByDate);
+        return Map.fromEntries(albums);
       case ChipSortFactor.name:
       default:
         final regularAlbums = <String>[], appAlbums = <String>[], specialAlbums = <String>[];
--- lib/widgets/filter_grids/tags_page.dart
+++ lib/widgets/filter_grids/tags_page.dart
@@ -1,10 +1,15 @@
 import 'package:aves/model/filters/tag.dart';
+import 'package:aves/model/image_entry.dart';
+import 'package:aves/model/settings/settings.dart';
 import 'package:aves/model/source/collection_source.dart';
+import 'package:aves/model/source/enums.dart';
 import 'package:aves/model/source/tag.dart';
 import 'package:aves/widgets/collection/empty.dart';
+import 'package:aves/widgets/common/aves_selection_dialog.dart';
 import 'package:aves/widgets/common/icons.dart';
 import 'package:aves/widgets/filter_grids/filter_grid_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TagListPage extends StatelessWidget {
   static const routeName = '/tags';
@@ -15,18 +20,63 @@ class TagListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: source.eventBus.on<TagsChangedEvent>(),
-      builder: (context, snapshot) => FilterNavigationPage(
-        source: source,
-        title: 'Tags',
-        filterEntries: source.getTagEntries(),
-        filterBuilder: (s) => TagFilter(s),
-        emptyBuilder: () => EmptyContent(
-          icon: AIcons.tag,
-          text: 'No tags',
-        ),
-      ),
+    return Selector<Settings, ChipSortFactor>(
+      selector: (context, s) => s.tagSortFactor,
+      builder: (context, sortFactor, child) {
+        return StreamBuilder(
+          stream: source.eventBus.on<TagsChangedEvent>(),
+          builder: (context, snapshot) => FilterNavigationPage(
+            source: source,
+            title: 'Tags',
+            onChipActionSelected: _onChipActionSelected,
+            filterEntries: _getTagEntries(),
+            filterBuilder: (s) => TagFilter(s),
+            emptyBuilder: () => EmptyContent(
+              icon: AIcons.tag,
+              text: 'No tags',
+            ),
+          ),
+        );
+      },
     );
+  }
+
+  void _onChipActionSelected(BuildContext context, ChipAction action) async {
+    switch (action) {
+      case ChipAction.sort:
+        final factor = await showDialog<ChipSortFactor>(
+          context: context,
+          builder: (context) => AvesSelectionDialog<ChipSortFactor>(
+            initialValue: settings.tagSortFactor,
+            options: {
+              ChipSortFactor.date: 'By date',
+              ChipSortFactor.name: 'By name',
+            },
+            title: 'Sort',
+          ),
+        );
+        if (factor != null) {
+          settings.tagSortFactor = factor;
+        }
+        break;
+    }
+  }
+
+  Map<String, ImageEntry> _getTagEntries() {
+    final entriesByDate = source.sortedEntriesForFilterList;
+    final tags = source.sortedTags
+        .map((tag) => MapEntry(
+              tag,
+              entriesByDate.firstWhere((entry) => entry.xmpSubjects.contains(tag), orElse: () => null),
+            ))
+        .toList();
+
+    switch (settings.tagSortFactor) {
+      case ChipSortFactor.date:
+        tags.sort(FilterNavigationPage.compareChipByDate);
+        break;
+      case ChipSortFactor.name:
+    }
+    return Map.fromEntries(tags);
   }
 }
