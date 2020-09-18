0e42568d85fe1040579769c9e315fad95d03e9bc
Thibault Deckers <thibault.deckers@gmail.com>
2020-09-18 20:41:30 +0900

    crashlytics: log locales, timezone, route changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:isolate';
+import 'dart:ui';
 
 import 'package:aves/model/settings/settings.dart';
+import 'package:aves/utils/route_tracker.dart';
 import 'package:aves/widgets/common/data_providers/settings_provider.dart';
 import 'package:aves/widgets/common/icons.dart';
 import 'package:aves/widgets/home_page.dart';
@@ -38,8 +40,29 @@ class AvesApp extends StatefulWidget {
 
 class _AvesAppState extends State<AvesApp> {
   Future<void> _appSetup;
+  final NavigatorObserver _routeTracker = CrashlyticsRouteTracker();
 
   static const accentColor = Colors.indigoAccent;
+
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    accentColor: accentColor,
+    scaffoldBackgroundColor: Colors.grey[900],
+    buttonColor: accentColor,
+    toggleableActiveColor: accentColor,
+    tooltipTheme: TooltipThemeData(
+      verticalOffset: 32,
+    ),
+    appBarTheme: AppBarTheme(
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Concourse Caps',
+        ),
+      ),
+    ),
+  );
 
   @override
   void initState() {
@@ -50,6 +73,9 @@ class _AvesAppState extends State<AvesApp> {
   Future<void> _setup() async {
     await Firebase.initializeApp().then((app) {
       FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+      FirebaseCrashlytics.instance.setCustomKey('locales', window.locales.join(', '));
+      final now = DateTime.now();
+      FirebaseCrashlytics.instance.setCustomKey('timezone', '${now.timeZoneName} (${now.timeZoneOffset})');
       FirebaseCrashlytics.instance.setCustomKey(
           'build_mode',
           kReleaseMode
@@ -65,44 +91,45 @@ class _AvesAppState extends State<AvesApp> {
   Widget build(BuildContext context) {
     // place the settings provider above `MaterialApp`
     // so it can be used during navigation transitions
+    final home = FutureBuilder<void>(
+      future: _appSetup,
+      builder: (context, snapshot) {
+        if (!snapshot.hasError && snapshot.connectionState == ConnectionState.done) {
+          return settings.hasAcceptedTerms ? HomePage() : WelcomePage();
+        }
+        return Scaffold(
+          body: snapshot.hasError
+              ? Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(AIcons.error),
+                      SizedBox(height: 16),
+                      Text(snapshot.error.toString()),
+                    ],
+                  ),
+                )
+              : SizedBox.shrink(),
+        );
+      },
+    );
     return SettingsProvider(
       child: OverlaySupport(
-        child: MaterialApp(
-          title: 'Aves',
-          theme: ThemeData(
-            brightness: Brightness.dark,
-            accentColor: accentColor,
-            scaffoldBackgroundColor: Colors.grey[900],
-            buttonColor: accentColor,
-            toggleableActiveColor: accentColor,
-            tooltipTheme: TooltipThemeData(
-              verticalOffset: 32,
-            ),
-            appBarTheme: AppBarTheme(
-              textTheme: TextTheme(
-                headline6: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Concourse Caps',
-                ),
-              ),
-            ),
-          ),
-          home: FutureBuilder<void>(
-            future: _appSetup,
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Column(
-                  children: [
-                    Icon(AIcons.error),
-                    Text(snapshot.error),
-                  ],
-                );
-              }
-              if (snapshot.connectionState != ConnectionState.done) return Scaffold();
-              return settings.hasAcceptedTerms ? HomePage() : WelcomePage();
-            },
-          ),
+        child: FutureBuilder<void>(
+          future: _appSetup,
+          builder: (context, snapshot) {
+            return MaterialApp(
+              home: home,
+              navigatorObservers: [
+                if (!snapshot.hasError && snapshot.connectionState == ConnectionState.done) _routeTracker,
+              ],
+              title: 'Aves',
+              darkTheme: darkTheme,
+              themeMode: ThemeMode.dark,
+            );
+          },
         ),
       ),
     );
