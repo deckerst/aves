055cad333f68b2a3620e6a7c68f2ccc94ac1ca6f
Thibault Deckers <thibault.deckers@gmail.com>
2020-09-20 17:02:50 +0900

    albums: pin to top

--- lib/widgets/filter_grids/chip_actions.dart
+++ lib/widgets/filter_grids/chip_actions.dart
@@ -1,3 +1,37 @@
+import 'package:aves/widgets/common/icons.dart';
+import 'package:flutter/widgets.dart';
+
 enum ChipAction {
   sort,
+}
+
+enum AlbumAction {
+  pin,
+  unpin,
+  rename,
+}
+
+extension ExtraAlbumAction on AlbumAction {
+  String getText() {
+    switch (this) {
+      case AlbumAction.pin:
+        return 'Pin to top';
+      case AlbumAction.unpin:
+        return 'Unpin from top';
+      case AlbumAction.rename:
+        return 'Rename';
+    }
+    return null;
+  }
+
+  IconData getIcon() {
+    switch (this) {
+      case AlbumAction.pin:
+      case AlbumAction.unpin:
+        return AIcons.pin;
+      case AlbumAction.rename:
+        return AIcons.rename;
+    }
+    return null;
+  }
 }
