1c671dc55419cf169634860a93bec36ac6c10f15
Thibault Deckers <thibault.deckers@gmail.com>
2020-10-05 20:57:51 +0900

    improved error logging

--- android/app/src/main/java/deckers/thibault/aves/decoder/VideoThumbnailGlideModule.java
+++ android/app/src/main/java/deckers/thibault/aves/decoder/VideoThumbnailGlideModule.java
@@ -5,29 +5,16 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.load.DecodeFormat;
-import com.bumptech.glide.module.AppGlideModule;
-import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.module.LibraryGlideModule;
 
 import java.io.InputStream;
 
 @GlideModule
-public class VideoThumbnailGlideModule extends AppGlideModule {
-    @Override
-    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
-        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_RGB_565));
-    }
-
+public class VideoThumbnailGlideModule extends LibraryGlideModule {
     @Override
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
         registry.append(VideoThumbnail.class, InputStream.class, new VideoThumbnailLoader.Factory());
-    }
-
-    @Override
-    public boolean isManifestParsingEnabled() {
-        return false;
     }
 }
