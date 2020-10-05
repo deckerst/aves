822bdc97627be3a7bc24ed27e687935823710d70
Thibault Deckers <thibault.deckers@gmail.com>
2020-10-05 21:50:23 +0900

    tests: fixed driver test

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,4 +1,6 @@
 import 'package:aves/main.dart' as app;
+import 'package:aves/model/settings/screen_on.dart';
+import 'package:aves/model/settings/settings.dart';
 import 'package:aves/services/android_file_service.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:path/path.dart' as path;
@@ -13,6 +15,13 @@ void main() {
   // we do it via the app instead of broadcasting via ADB
   // because `MEDIA_SCANNER_SCAN_FILE` intent got deprecated in API 29
   unawaited(AndroidFileService.scanFile(path.join(targetPicturesDir, 'ipse.jpg'), 'image/jpeg'));
+
+  configureAndLaunch();
+}
+
+Future<void> configureAndLaunch() async {
+  await settings.init();
+  settings.keepScreenOn = KeepScreenOn.always;
 
   app.main();
 }
