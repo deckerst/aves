8fc366cd89af60c5b75acef5b4a70d00ec696d03
Thibault Deckers <thibault.deckers@gmail.com>
2020-10-20 10:58:47 +0900

    Kotlin migration (WIP)

---
+++ android/app/src/main/kotlin/deckers/thibault/aves/model/provider/ImageProviderFactory.kt
@@ -0,0 +1,24 @@
+package deckers.thibault.aves.model.provider
+
+import android.content.ContentResolver
+import android.net.Uri
+import android.provider.MediaStore
+import java.util.*
+
+object ImageProviderFactory {
+    @JvmStatic
+    fun getProvider(uri: Uri): ImageProvider? {
+        return when (uri.scheme?.toLowerCase(Locale.ROOT)) {
+            ContentResolver.SCHEME_CONTENT -> {
+                // a URI's authority is [userinfo@]host[:port]
+                // but we only want the host when comparing to Media Store's "authority"
+                return when (uri.host?.toLowerCase(Locale.ROOT)) {
+                    MediaStore.AUTHORITY -> MediaStoreImageProvider()
+                    else -> ContentImageProvider()
+                }
+            }
+            ContentResolver.SCHEME_FILE -> FileImageProvider()
+            else -> null
+        }
+    }
+}
