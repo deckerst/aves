296871bb9f8003e97f6a774035b517443728dc87
Thibault Deckers <thibault.deckers@gmail.com>
2020-12-08 11:19:52 +0900

    info: format XMP keys and some values (enums in Exif/Photoshop/TIFF)

---
+++ lib/widgets/fullscreen/info/metadata/xmp_ns/iptc.dart
@@ -0,0 +1,25 @@
+import 'package:aves/widgets/fullscreen/info/metadata/xmp_namespaces.dart';
+import 'package:aves/widgets/fullscreen/info/metadata/xmp_structs.dart';
+import 'package:flutter/material.dart';
+
+class XmpIptcCoreNamespace extends XmpNamespace {
+  static const ns = 'Iptc4xmpCore';
+
+  static final creatorContactInfoPattern = RegExp(r'Iptc4xmpCore:CreatorContactInfo/(.*)');
+
+  final creatorContactInfo = <String, String>{};
+
+  XmpIptcCoreNamespace() : super(ns);
+
+  @override
+  bool extractData(XmpProp prop) => extractStruct(prop, creatorContactInfoPattern, creatorContactInfo);
+
+  @override
+  List<Widget> buildFromExtractedData() => [
+        if (creatorContactInfo.isNotEmpty)
+          XmpStructCard(
+            title: 'Creator Contact Info',
+            struct: creatorContactInfo,
+          ),
+      ];
+}
