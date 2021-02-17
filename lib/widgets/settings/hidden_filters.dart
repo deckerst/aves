f16d98ba2b74f43870eb6e5f3373af5c15f2b244
Thibault Deckers <thibault.deckers@gmail.com>
2021-02-17 11:44:59 +0900

    settings: changed layout for hidden filters and access grants

--- lib/widgets/settings/hidden_filters.dart
+++ lib/widgets/settings/hidden_filters.dart
@@ -1,34 +1,26 @@
-import 'package:aves/model/filters/filters.dart';
 import 'package:aves/model/settings/settings.dart';
 import 'package:aves/model/source/collection_source.dart';
+import 'package:aves/theme/icons.dart';
+import 'package:aves/widgets/collection/empty.dart';
 import 'package:aves/widgets/common/identity/aves_filter_chip.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class HiddenFilters extends StatelessWidget {
+class HiddenFilterTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Selector<Settings, Set<CollectionFilter>>(
-        selector: (context, s) => s.hiddenFilters,
-        builder: (context, hiddenFilters, child) {
-          return ListTile(
-            title: hiddenFilters.isEmpty ? Text('There are no hidden filters') : Text('Hidden filters'),
-            trailing: hiddenFilters.isEmpty
-                ? null
-                : OutlinedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          settings: RouteSettings(name: HiddenFilterPage.routeName),
-                          builder: (context) => HiddenFilterPage(),
-                        ),
-                      );
-                    },
-                    child: Text('Edit'.toUpperCase()),
-                  ),
-          );
-        });
+    return ListTile(
+      title: Text('Hidden filters'),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            settings: RouteSettings(name: HiddenFilterPage.routeName),
+            builder: (context) => HiddenFilterPage(),
+          ),
+        );
+      },
+    );
   }
 }
 
@@ -42,24 +34,49 @@ class HiddenFilterPage extends StatelessWidget {
         title: Text('Hidden Filters'),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Consumer<Settings>(
-            builder: (context, settings, child) {
-              final filterList = settings.hiddenFilters.toList()..sort();
-              return Wrap(
-                spacing: 8,
-                runSpacing: 8,
-                children: filterList
-                    .map((filter) => AvesFilterChip(
-                          filter: filter,
-                          removable: true,
-                          onTap: (filter) => context.read<CollectionSource>().changeFilterVisibility(filter, true),
-                        ))
-                    .toList(),
-              );
-            },
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: Row(
+                children: [
+                  Icon(AIcons.info),
+                  SizedBox(width: 16),
+                  Expanded(child: Text('Photos and videos matching hidden filters will not appear in your collection.')),
+                ],
+              ),
+            ),
+            Divider(),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Consumer<Settings>(
+                  builder: (context, settings, child) {
+                    final hiddenFilters = settings.hiddenFilters;
+                    final filterList = hiddenFilters.toList()..sort();
+                    if (hiddenFilters.isEmpty) {
+                      return EmptyContent(
+                        icon: AIcons.hide,
+                        text: 'No hidden filters',
+                      );
+                    }
+                    return Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      children: filterList
+                          .map((filter) => AvesFilterChip(
+                                filter: filter,
+                                removable: true,
+                                onTap: (filter) => context.read<CollectionSource>().changeFilterVisibility(filter, true),
+                              ))
+                          .toList(),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
