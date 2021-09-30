b605cb62c7c198685ed6301851bd6d0e91611e96
Thibault Deckers <thibault.deckers@gmail.com>
2021-09-30 17:27:16 +0900

    #72 fixed hiding root album of hidden path

--- test/model/filters_test.dart
+++ test/model/filters_test.dart
@@ -3,12 +3,27 @@ import 'package:aves/model/filters/favourite.dart';
 import 'package:aves/model/filters/filters.dart';
 import 'package:aves/model/filters/location.dart';
 import 'package:aves/model/filters/mime.dart';
+import 'package:aves/model/filters/path.dart';
 import 'package:aves/model/filters/query.dart';
 import 'package:aves/model/filters/tag.dart';
 import 'package:aves/model/filters/type.dart';
+import 'package:aves/services/common/services.dart';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
+import '../fake/media_store_service.dart';
+import '../fake/storage_service.dart';
+
 void main() {
+  setUp(() async {
+    // specify Posix style path context for consistent behaviour when running tests on Windows
+    getIt.registerLazySingleton<p.Context>(() => p.Context(style: p.Style.posix));
+  });
+
+  tearDown(() async {
+    await getIt.reset();
+  });
+
   test('Filter serialization', () {
     CollectionFilter? jsonRoundTrip(filter) => CollectionFilter.fromJson(filter.toJson());
 
@@ -21,16 +36,34 @@ void main() {
     final location = LocationFilter(LocationLevel.country, 'France${LocationFilter.locationSeparator}FR');
     expect(location, jsonRoundTrip(location));
 
-    final type = TypeFilter.sphericalVideo;
-    expect(type, jsonRoundTrip(type));
-
     final mime = MimeFilter.video;
     expect(mime, jsonRoundTrip(mime));
+
+    final path = PathFilter('/some/path/');
+    expect(path, jsonRoundTrip(path));
 
     final query = QueryFilter('some query');
     expect(query, jsonRoundTrip(query));
 
     final tag = TagFilter('some tag');
     expect(tag, jsonRoundTrip(tag));
+
+    final type = TypeFilter.sphericalVideo;
+    expect(type, jsonRoundTrip(type));
+  });
+
+  test('Path filter', () {
+    const rootAlbum = '${FakeStorageService.primaryPath}Pictures/test';
+    const subAlbum = '${FakeStorageService.primaryPath}Pictures/test/sub';
+    const siblingAlbum = '${FakeStorageService.primaryPath}Pictures/test sibling';
+
+    final rootImage = FakeMediaStoreService.newImage(rootAlbum, 'image1');
+    final subImage = FakeMediaStoreService.newImage(subAlbum, 'image1');
+    final siblingImage = FakeMediaStoreService.newImage(siblingAlbum, 'image1');
+
+    final path = PathFilter('$rootAlbum/');
+    expect(path.test(rootImage), true);
+    expect(path.test(subImage), true);
+    expect(path.test(siblingImage), false);
   });
 }
