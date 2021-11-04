7ec95c5afd34024668639ae1e0e00b0c7c77129c
Thibault Deckers <thibault.deckers@gmail.com>
2021-11-04 20:20:25 +0900

    test fix

--- test/model/video/metadata_test.dart
+++ test/model/video/metadata_test.dart
@@ -3,9 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('Video date parsing', () {
-    final localOffset = DateTime.now().timeZoneOffset;
-
-    expect(VideoMetadataFormatter.parseVideoDate('2011-05-08T03:46+09:00'), DateTime(2011, 5, 8, 12, 46).add(-localOffset).millisecondsSinceEpoch);
+    expect(VideoMetadataFormatter.parseVideoDate('2011-05-08T03:46+09:00'), DateTime(2011, 5, 8, 3, 46).millisecondsSinceEpoch);
     expect(VideoMetadataFormatter.parseVideoDate('UTC 2021-05-30 19:14:21'), DateTime(2021, 5, 30, 19, 14, 21).millisecondsSinceEpoch);
   });
 }
