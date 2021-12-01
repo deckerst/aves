80faf954475b9d493932d4383fa0d6a1c7bfd26c
Thibault Deckers <thibault.deckers@gmail.com>
2021-12-01 17:38:20 +0900

    minor fix

--- lib/widgets/settings/language/locale.dart
+++ lib/widgets/settings/language/locale.dart
@@ -8,6 +8,7 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class LocaleTile extends StatelessWidget {
   static const _systemLocaleOption = Locale('system');
@@ -16,10 +17,14 @@ class LocaleTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final current = settings.locale;
     return ListTile(
       title: Text(context.l10n.settingsLanguage),
-      subtitle: Text(current == null ? context.l10n.settingsSystemDefault : _getLocaleName(current)),
+      subtitle: Selector<Settings, Locale?>(
+        selector: (context, s) => settings.locale,
+        builder: (context, locale, child) {
+          return Text(locale == null ? context.l10n.settingsSystemDefault : _getLocaleName(locale));
+        },
+      ),
       onTap: () async {
         final value = await showDialog<Locale>(
           context: context,
