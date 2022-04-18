8e0b1b495e19a7b7a59e80ec210ddc31f1baadb5
Thibault Deckers <thibault.deckers@gmail.com>
2022-04-18 18:49:25 +0900

    #225 bin: restore action in snack bar following move

--- lib/widgets/map/map_page.dart
+++ lib/widgets/map/map_page.dart
@@ -26,7 +26,7 @@ import 'package:aves/widgets/common/providers/media_query_data_provider.dart';
 import 'package:aves/widgets/common/thumbnail/scroller.dart';
 import 'package:aves/widgets/map/map_info_row.dart';
 import 'package:aves/widgets/viewer/entry_viewer_page.dart';
-import 'package:aves/widgets/viewer/info/notifications.dart';
+import 'package:aves/widgets/viewer/notifications.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
--- lib/widgets/viewer/action/entry_action_delegate.dart
+++ lib/widgets/viewer/action/entry_action_delegate.dart
@@ -31,7 +31,7 @@ import 'package:aves/widgets/filter_grids/album_pick.dart';
 import 'package:aves/widgets/viewer/action/printer.dart';
 import 'package:aves/widgets/viewer/action/single_entry_editor.dart';
 import 'package:aves/widgets/viewer/debug/debug_page.dart';
-import 'package:aves/widgets/viewer/info/notifications.dart';
+import 'package:aves/widgets/viewer/notifications.dart';
 import 'package:aves/widgets/viewer/overlay/notifications.dart';
 import 'package:aves/widgets/viewer/source_viewer_page.dart';
 import 'package:aves/widgets/viewer/video/conductor.dart';
@@ -205,7 +205,7 @@ class EntryActionDelegate with FeedbackMixin, PermissionAwareMixin, SizeAwareMix
       if (source.initState != SourceInitializationState.none) {
         await source.removeEntries({entry.uri}, includeTrash: true);
       }
-      EntryRemovedNotification(entry).dispatch(context);
+      EntryDeletedNotification({entry}).dispatch(context);
     }
   }
 
@@ -299,20 +299,11 @@ class EntryActionDelegate with FeedbackMixin, PermissionAwareMixin, SizeAwareMix
     );
   }
 
-  Future<void> _move(BuildContext context, {required MoveType moveType}) async {
-    await move(
-      context,
-      moveType: moveType,
-      entries: {entry},
-      onSuccess: {
-        MoveType.move,
-        MoveType.toBin,
-        MoveType.fromBin,
-      }.contains(moveType)
-          ? () => EntryRemovedNotification(entry).dispatch(context)
-          : null,
-    );
-  }
+  Future<void> _move(BuildContext context, {required MoveType moveType}) => move(
+        context,
+        moveType: moveType,
+        entries: {entry},
+      );
 
   Future<void> _rename(BuildContext context) async {
     final newName = await showDialog<String>(
