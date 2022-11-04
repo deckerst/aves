22149ffca2289871d0ce86a03bcfe8e2aa8082ee
Thibault Deckers <thibault.deckers@gmail.com>
2022-11-04 23:50:26 +0100

    #383 optional media management permission

--- lib/services/device_service.dart
+++ lib/services/device_service.dart
@@ -4,6 +4,8 @@ import 'package:aves/services/common/services.dart';
 import 'package:flutter/services.dart';
 
 abstract class DeviceService {
+  Future<bool> canManageMedia();
+
   Future<Map<String, dynamic>> getCapabilities();
 
   Future<String?> getDefaultTimeZone();
@@ -13,10 +15,23 @@ abstract class DeviceService {
   Future<int> getPerformanceClass();
 
   Future<bool> isSystemFilePickerEnabled();
+
+  Future<void> requestMediaManagePermission();
 }
 
 class PlatformDeviceService implements DeviceService {
   static const _platform = MethodChannel('deckers.thibault/aves/device');
+
+  @override
+  Future<bool> canManageMedia() async {
+    try {
+      final result = await _platform.invokeMethod('canManageMedia');
+      if (result != null) return result as bool;
+    } on PlatformException catch (e, stack) {
+      await reportService.recordError(e, stack);
+    }
+    return false;
+  }
 
   @override
   Future<Map<String, dynamic>> getCapabilities() async {
@@ -79,5 +94,14 @@ class PlatformDeviceService implements DeviceService {
       await reportService.recordError(e, stack);
     }
     return false;
+  }
+
+  @override
+  Future<void> requestMediaManagePermission() async {
+    try {
+      await _platform.invokeMethod('requestMediaManagePermission');
+    } on PlatformException catch (e, stack) {
+      await reportService.recordError(e, stack);
+    }
   }
 }
