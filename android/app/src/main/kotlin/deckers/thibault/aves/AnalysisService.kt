041d89d486f12160babfdc85cba7cbc6c2229343
Thibault Deckers <thibault.deckers@gmail.com>
2023-02-17 16:38:50 +0100

    minor fixes

--- android/app/src/main/kotlin/deckers/thibault/aves/AnalysisService.kt
+++ android/app/src/main/kotlin/deckers/thibault/aves/AnalysisService.kt
@@ -37,12 +37,16 @@ class AnalysisService : Service() {
             }
         }
 
-        initChannels(this)
+        try {
+            initChannels(this)
 
-        HandlerThread("Analysis service handler", Process.THREAD_PRIORITY_BACKGROUND).apply {
-            start()
-            serviceLooper = looper
-            serviceHandler = ServiceHandler(looper)
+            HandlerThread("Analysis service handler", Process.THREAD_PRIORITY_BACKGROUND).apply {
+                start()
+                serviceLooper = looper
+                serviceHandler = ServiceHandler(looper)
+            }
+        } catch (e: Exception) {
+            Log.e(LOG_TAG, "failed to initialize service", e)
         }
     }
 
