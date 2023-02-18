a1990e44aeabebe7bed1f818a6665b1be45c7514
Thibault Deckers <thibault.deckers@gmail.com>
2023-02-18 17:22:15 +0100

    tv: improved view config dialog in landscape

--- lib/widgets/dialogs/aves_dialog.dart
+++ lib/widgets/dialogs/aves_dialog.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:aves/model/settings/settings.dart';
 import 'package:aves/widgets/common/extensions/build_context.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AvesDialog extends StatelessWidget {
   static const confirmationRouteName = '/dialog/confirmation';
@@ -103,7 +104,7 @@ class AvesDialog extends StatelessWidget {
         // workaround because the dialog tries
         // to size itself to the content intrinsic size,
         // but the `ListView` viewport does not have one
-        width: 1,
+        width: context.select<MediaQueryData, double>((mq) => mq.size.width / 2),
         child: DecoratedBox(
           decoration: contentDecoration(context),
           child: child,
