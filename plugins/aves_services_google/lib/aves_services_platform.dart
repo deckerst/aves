c0cfa1ab957b608a3ef812a76cfee046fb05f2db
Thibault Deckers <thibault.deckers@gmail.com>
2023-04-05 19:10:57 +0200

    #241 upgrade, checked google maps issue

--- plugins/aves_services_google/lib/aves_services_platform.dart
+++ plugins/aves_services_google/lib/aves_services_platform.dart
@@ -6,8 +6,6 @@ import 'package:aves_services_platform/src/map.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_api_availability/google_api_availability.dart';
-import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 import 'package:latlong2/latlong.dart' as ll;
 
 class PlatformMobileServices extends MobileServices {
@@ -20,26 +18,8 @@ class PlatformMobileServices extends MobileServices {
     _isAvailable = result == GooglePlayServicesAvailability.success;
     debugPrint('Device has Google Play Services=$_isAvailable');
 
-    // as of google_maps_flutter v2.1.1, minSDK is 20 because of default PlatformView usage,
-    // but using hybrid composition would make it usable on API 19 too,
-    // cf https://github.com/flutter/flutter/issues/23728
-    // as of google_maps_flutter v2.1.5, Flutter v3.0.1 makes the map hide overlay widgets on API <=22
     final androidInfo = await DeviceInfoPlugin().androidInfo;
     _canRenderMaps = androidInfo.version.sdkInt >= 21;
-    if (_canRenderMaps) {
-      final mapsImplementation = GoogleMapsFlutterPlatform.instance;
-      if (mapsImplementation is GoogleMapsFlutterAndroid) {
-        // as of google_maps_flutter_android v2.2.0,
-        // setting `useAndroidViewSurface` to true:
-        // + issue #241 exists but workaround is efficient
-        // + pan perf is OK when overlay is disabled
-        // - pan perf is bad when overlay is enabled
-        // setting `useAndroidViewSurface` to false:
-        // - issue #241 exists and workaround is inefficient
-        // + pan perf is OK when overlay is disabled or enabled
-        mapsImplementation.useAndroidViewSurface = false;
-      }
-    }
   }
 
   @override
