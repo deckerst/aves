4106fd887e0f7b76f914a1a34c8dc3bda1c8a1b7
Thibault Deckers <thibault.deckers@gmail.com>
2023-04-17 13:20:03 +0200

    gradle upgrade

---
+++ plugins/aves_screen_state/android/src/main/kotlin/deckers/thibault/aves/aves_screen_state/AvesScreenStatePlugin.kt
@@ -0,0 +1,40 @@
+package deckers.thibault.aves.aves_screen_state
+
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
+
+class AvesScreenStatePlugin : FlutterPlugin, EventChannel.StreamHandler {
+    private lateinit var eventChannel: EventChannel
+    private var context: Context? = null
+    private var screenReceiver: ScreenReceiver? = null
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "deckers.thibault/aves_screen_state/events")
+        context = flutterPluginBinding.applicationContext
+        eventChannel.setStreamHandler(this)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        eventChannel.setStreamHandler(null)
+    }
+
+    override fun onListen(arguments: Any?, events: EventSink) {
+        screenReceiver = ScreenReceiver(events)
+
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_SCREEN_ON) // Turn on
+            addAction(Intent.ACTION_SCREEN_OFF) // Turn off
+            addAction(Intent.ACTION_USER_PRESENT) // Unlock
+        }
+        context!!.registerReceiver(screenReceiver, filter)
+    }
+
+    override fun onCancel(arguments: Any?) {
+        context!!.unregisterReceiver(screenReceiver)
+    }
+}
