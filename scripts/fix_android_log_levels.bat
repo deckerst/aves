c75c75fc4d3508b8eb44cb61628b4e56b9293e47
Thibault Deckers <thibault.deckers@gmail.com>
2023-04-26 23:51:01 +0200

    minor

--- scripts/fix_android_log_levels.bat
+++ scripts/fix_android_log_levels.bat
@@ -11,6 +11,12 @@ adb.exe shell setprop log.tag.ACodec WARN
 adb.exe shell setprop log.tag.AHierarchicalStateMachine ERROR
 adb.exe shell setprop log.tag.AudioCapabilities ERROR
 adb.exe shell setprop log.tag.AudioTrack INFO
+adb.exe shell setprop log.tag.BufferPoolAccessor2.0 INFO
+adb.exe shell setprop log.tag.CCodec INFO
+adb.exe shell setprop log.tag.CCodecBufferChannel INFO
+adb.exe shell setprop log.tag.CCodecBuffers INFO
+adb.exe shell setprop log.tag.CCodecConfig INFO
+adb.exe shell setprop log.tag.Codec2Client INFO
 adb.exe shell setprop log.tag.CompatibilityChangeReporter INFO
 adb.exe shell setprop log.tag.Counters WARN
 adb.exe shell setprop log.tag.CustomizedTextParser INFO
@@ -24,6 +30,8 @@ adb.exe shell setprop log.tag.MediaCodec WARN
 adb.exe shell setprop log.tag.MediaMetadataRetriever INFO
 adb.exe shell setprop log.tag.MediaMetadataRetrieverJNI INFO
 adb.exe shell setprop log.tag.NuMediaExtractor INFO
+adb.exe shell setprop log.tag.PipelineWatcher INFO
+adb.exe shell setprop log.tag.ReflectedParamUpdater INFO
 adb.exe shell setprop log.tag.skia INFO
 adb.exe shell setprop log.tag.SurfaceControl WARN
 adb.exe shell setprop log.tag.SurfaceUtils INFO
--- scripts/fix_android_log_levels.sh
+++ scripts/fix_android_log_levels.sh
@@ -3,6 +3,12 @@ adb shell setprop log.tag.ACodec WARN
 adb shell setprop log.tag.AHierarchicalStateMachine ERROR
 adb shell setprop log.tag.AudioCapabilities ERROR
 adb shell setprop log.tag.AudioTrack INFO
+adb shell setprop log.tag.BufferPoolAccessor2.0 INFO
+adb shell setprop log.tag.CCodec INFO
+adb shell setprop log.tag.CCodecBufferChannel INFO
+adb shell setprop log.tag.CCodecBuffers INFO
+adb shell setprop log.tag.CCodecConfig INFO
+adb shell setprop log.tag.Codec2Client INFO
 adb shell setprop log.tag.CompatibilityChangeReporter INFO
 adb shell setprop log.tag.Counters WARN
 adb shell setprop log.tag.CustomizedTextParser INFO
@@ -16,6 +22,8 @@ adb shell setprop log.tag.MediaCodec WARN
 adb shell setprop log.tag.MediaMetadataRetriever INFO
 adb shell setprop log.tag.MediaMetadataRetrieverJNI INFO
 adb shell setprop log.tag.NuMediaExtractor INFO
+adb shell setprop log.tag.PipelineWatcher INFO
+adb shell setprop log.tag.ReflectedParamUpdater INFO
 adb shell setprop log.tag.skia INFO
 adb shell setprop log.tag.SurfaceControl WARN
 adb shell setprop log.tag.SurfaceUtils INFO
