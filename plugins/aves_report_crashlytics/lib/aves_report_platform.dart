6aea396fe9bc8d018a3e97c55d33bda3e1a2a9af
Thibault Deckers <thibault.deckers@gmail.com>
2023-05-23 00:30:07 +0200

    exception report split for crashlytics

--- plugins/aves_report_crashlytics/lib/aves_report_platform.dart
+++ plugins/aves_report_crashlytics/lib/aves_report_platform.dart
@@ -3,12 +3,10 @@ library aves_report_platform;
 import 'dart:async';
 
 import 'package:aves_report/aves_report.dart';
-import 'package:collection/collection.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:stack_trace/stack_trace.dart';
 
 class PlatformReportService extends ReportService {
   FirebaseCrashlytics? get _instance {
@@ -71,17 +69,7 @@ class PlatformReportService extends ReportService {
   @override
   Future<void> recordError(dynamic exception, StackTrace? stack) async {
     if (exception is PlatformException && stack != null) {
-      // simply creating a trace with `Trace.current(1)` or creating a `Trace` from modified frames
-      // does not yield a stack trace that Crashlytics can segment,
-      // so we reconstruct a string stack trace instead
-      stack = StackTrace.fromString(Trace.from(stack)
-          .frames
-          .skip(2)
-          .toList()
-          .mapIndexed(
-            (i, f) => '#${(i++).toString().padRight(8)}${f.member} (${f.uri}:${f.line}:${f.column})',
-          )
-          .join('\n'));
+      stack = ReportService.buildReportStack(stack, level: 2);
     }
     return _instance?.recordError(exception, stack);
   }
