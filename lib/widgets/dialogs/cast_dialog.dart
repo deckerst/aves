c505dfb12013825fd0ce56729a75d95cc021d328
Thibault Deckers <thibault.deckers@gmail.com>
2023-10-27 18:19:20 +0300

    Revert "upnp2 test"
    This reverts commit 902ceca2681e5764f7f1293b41f6dab4f8c00fa5.

--- lib/widgets/dialogs/cast_dialog.dart
+++ lib/widgets/dialogs/cast_dialog.dart
@@ -1,10 +1,7 @@
-import 'dart:async';
-
-import 'package:aves/ref/upnp.dart';
 import 'package:aves/widgets/common/extensions/build_context.dart';
 import 'package:aves/widgets/dialogs/aves_dialog.dart';
+import 'package:dlna_dart/dlna.dart';
 import 'package:flutter/material.dart';
-import 'package:upnp2/upnp.dart';
 
 class CastDialog extends StatefulWidget {
   static const routeName = '/dialog/cast';
@@ -16,60 +13,26 @@ class CastDialog extends StatefulWidget {
 }
 
 class _CastDialogState extends State<CastDialog> {
-  final DeviceDiscoverer _discoverer = DeviceDiscoverer();
-  Timer? _discoverySearchTimer;
-  int _queryIndex = 0;
-  final Map<String, Device> _seenRenderers = {};
+  final DLNAManager _dlnaManager = DLNAManager();
+  final Map<String, DLNADevice> _seenRenderers = {};
+
+  static const String upnpDeviceTypeMediaRenderer = 'urn:schemas-upnp-org:device:MediaRenderer:1';
 
   @override
   void initState() {
     super.initState();
-    _discoverClients(
-      [
-        Upnp.ssdpQueryAll,
-        Upnp.upnpServiceTypeAVTransport,
-        Upnp.upnpDeviceTypeMediaRenderer,
-      ],
-    ).listen((client) async {
-      try {
-        final device = await client.getDevice();
-        if (device != null) {
-          final uuid = device.uuid;
-          if (uuid != null && device.deviceType == Upnp.upnpDeviceTypeMediaRenderer) {
-            _seenRenderers[uuid] = device;
-            setState(() {});
-          }
-        }
-      } catch (e) {
-        debugPrint('TLAD failed to get device e=$e');
-      }
+
+    _dlnaManager.start().then((deviceManager) {
+      deviceManager.devices.stream.listen((devices) {
+        _seenRenderers.addAll(Map.fromEntries(devices.entries.where((kv) => kv.value.info.deviceType == upnpDeviceTypeMediaRenderer)));
+        setState(() {});
+      });
     });
-  }
-
-  Stream<DiscoveredClient> _discoverClients(List<String> queries) async* {
-    await _discoverer.start(
-      ipv6: false,
-      onError: (e) => debugPrint('cast: failed to start discoverer with error=$e'),
-    );
-    _search(queries);
-    _discoverySearchTimer = Timer.periodic(const Duration(seconds: 5), (_) => _search(queries));
-    await for (var client in _discoverer.clients) {
-      yield client;
-    }
-  }
-
-  void _search(List<String> queries) {
-    final searchTarget = queries[_queryIndex];
-    debugPrint('cast: search target=$searchTarget');
-    _discoverer.search(searchTarget);
-    _queryIndex = (_queryIndex + 1) % queries.length;
   }
 
   @override
   void dispose() {
-    _discoverySearchTimer?.cancel();
-    _discoverySearchTimer = null;
-    _discoverer.stop();
+    _dlnaManager.stop();
     super.dispose();
   }
 
@@ -86,8 +49,8 @@ class _CastDialogState extends State<CastDialog> {
             ),
           ),
         ..._seenRenderers.values.map((dev) => ListTile(
-              title: Text(dev.friendlyName ?? dev.uuid!),
-              onTap: () => Navigator.maybeOf(context)?.pop<Device>(dev),
+              title: Text(dev.info.friendlyName),
+              onTap: () => Navigator.maybeOf(context)?.pop<DLNADevice>(dev),
             )),
       ],
       actions: const [
