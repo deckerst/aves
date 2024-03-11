12bd53f511b5a547c0a0d809fbe609eb90033658
Thibault Deckers <thibault.deckers@gmail.com>
2024-03-11 23:21:39 +0100

    fixed tests

--- test/fake/device_service.dart
+++ test/fake/device_service.dart
@@ -5,4 +5,10 @@ import 'package:test/fake.dart';
 class FakeDeviceService extends Fake implements DeviceService {
   @override
   Future<int> getDefaultTimeZoneRawOffsetMillis() => SynchronousFuture(3600000);
+
+  @override
+  Future<int> getAvailableHeapSize() => SynchronousFuture(0x7fffffff);
+
+  @override
+  Future<void> requestGarbageCollection() => SynchronousFuture(null);
 }
