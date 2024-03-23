0a2e74af4bfae1c724bb2182f1a54ad5db3147d5
Thibault Deckers <thibault.deckers@gmail.com>
2024-03-23 14:27:38 +0100

    #956 fairphone burst pattern

--- lib/ref/bursts.dart
+++ lib/ref/bursts.dart
@@ -1,16 +1,19 @@
 class BurstPatterns {
   static const _keyGroupName = 'key';
 
+  static const fairphone = r'^IMG_(?<key>\d{8}_\d{6})_BURST(\d+)$';
   static const samsung = r'^(?<key>\d{8}_\d{6})_(\d+)$';
   static const sony = r'^DSC(PDC)?_\d+_BURST(?<key>\d{17})(_COVER)?$';
 
   static final options = [
+    BurstPatterns.fairphone,
     BurstPatterns.samsung,
     BurstPatterns.sony,
   ];
 
   static String getName(String pattern) {
     return switch (pattern) {
+      fairphone => 'Fairphone',
       samsung => 'Samsung',
       sony => 'Sony',
       _ => pattern,
@@ -19,6 +22,7 @@ class BurstPatterns {
 
   static String getExample(String pattern) {
     return switch (pattern) {
+      fairphone => 'IMG_20151021_072800_BURST007',
       samsung => '20151021_072800_007',
       sony => 'DSC_0007_BURST20151021072800123',
       _ => '?',
@@ -26,6 +30,7 @@ class BurstPatterns {
   }
 
   static const byManufacturer = {
+    _Manufacturers.fairphone: fairphone,
     _Manufacturers.samsung: samsung,
     _Manufacturers.sony: sony,
   };
@@ -47,8 +52,9 @@ class BurstPatterns {
   }
 }
 
-// values as returned by `DeviceInfoPlugin().androidInfo`
+// values as returned by `DeviceInfoPlugin().androidInfo` (lower-cased)
 class _Manufacturers {
+  static const fairphone = 'fairphone';
   static const samsung = 'samsung';
   static const sony = 'sony';
 }
