554e1faf8866fae988315f8af25e95ba3a30ebc9
Thibault Deckers <thibault.deckers@gmail.com>
2024-08-05 21:01:55 +0200

    #1113 viewer: long descriptions are scrollable when overlay is expanded by tap

--- lib/widgets/viewer/overlay/details/expander.dart
+++ lib/widgets/viewer/overlay/details/expander.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class OverlayRowExpander extends StatelessWidget {
+class OverlayRowExpander extends StatefulWidget {
   final ValueNotifier<bool> expandedNotifier;
   final Widget child;
 
@@ -11,23 +12,68 @@ class OverlayRowExpander extends StatelessWidget {
   });
 
   @override
+  State<OverlayRowExpander> createState() => _OverlayRowExpanderState();
+}
+
+class _OverlayRowExpanderState extends State<OverlayRowExpander> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void didUpdateWidget(covariant OverlayRowExpander oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.child != widget.child) {
+      if (_scrollController.hasClients && _scrollController.positions.every((v) => v.hasContentDimensions)) {
+        _scrollController.jumpTo(0);
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<bool>(
-      valueListenable: expandedNotifier,
+      valueListenable: widget.expandedNotifier,
       builder: (context, expanded, child) {
         final parent = DefaultTextStyle.of(context);
-        return DefaultTextStyle(
-          key: key,
+        child = DefaultTextStyle(
           style: parent.style,
           textAlign: parent.textAlign,
           softWrap: expanded,
           overflow: parent.overflow,
-          maxLines: expanded ? 16 : 1,
+          maxLines: expanded ? null : 1,
           textWidthBasis: parent.textWidthBasis,
           child: child!,
         );
+        if (expanded) {
+          child = ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: context.select<MediaQueryData, double>((mq) => mq.size.height / 5),
+            ),
+            child: MediaQuery.removePadding(
+              // remove padding so that scroll bar is consistent with the scroll view
+              context: context,
+              removeTop: true,
+              removeBottom: true,
+              child: Scrollbar(
+                controller: _scrollController,
+                thumbVisibility: true,
+                radius: const Radius.circular(16),
+                child: SingleChildScrollView(
+                  controller: _scrollController,
+                  child: child,
+                ),
+              ),
+            ),
+          );
+        }
+        return child;
       },
-      child: child,
+      child: widget.child,
     );
   }
 }
