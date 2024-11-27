d02e6cc69383da058928c0c5a2dc0625be498799
Thibault Deckers <thibault.deckers@gmail.com>
2024-11-28 00:13:56 +0100

    l10n

--- lib/model/app/contributors.dart
+++ lib/model/app/contributors.dart
@@ -115,6 +115,7 @@ class Contributors {
     Contributor('Hasan Kara', 'hasanbeytullahkara@gmail.com'),
     // Contributor('Femini', 'nizamismidov4@gmail.com'), // Azerbaijani
     // Contributor('Alvi Khan', 'aveenalvi@gmail.com'), // Bengali
+    // Contributor('Petrov', 'i_v_c@mail.ru'), // Bulgarian
     // Contributor('Htet Oo Hlaing', 'htetoh2006@outlook.com'), // Burmese
     // Contributor('Khant', 'khant@users.noreply.hosted.weblate.org'), // Burmese
     // Contributor('Grooty12', 'Rasmus@rosendahl-kaa.name'), // Danish
