4b4d444884f78b292646283474fed705d6c61f84
Thibault Deckers <thibault.deckers@gmail.com>
2025-02-04 19:30:06 +0100

    minor

--- android/app/src/main/kotlin/deckers/thibault/aves/model/AvesEntry.kt
+++ android/app/src/main/kotlin/deckers/thibault/aves/model/AvesEntry.kt
@@ -4,17 +4,17 @@ import android.net.Uri
 import androidx.core.net.toUri
 
 class AvesEntry(map: FieldMap) {
-    val uri: Uri = (map["uri"] as String).toUri() // content or file URI
-    val path = map["path"] as String? // best effort to get local path
-    val pageId = map["pageId"] as Int? // null means the main entry
-    val mimeType = map["mimeType"] as String
-    val width = map["width"] as Int
-    val height = map["height"] as Int
-    val rotationDegrees = map["rotationDegrees"] as Int
-    val isFlipped = map["isFlipped"] as Boolean
-    val sizeBytes = toLong(map["sizeBytes"])
-    val trashed = map["trashed"] as Boolean
-    val trashPath = map["trashPath"] as String?
+    val uri: Uri = (map[EntryFields.URI] as String).toUri() // content or file URI
+    val path = map[EntryFields.PATH] as String? // best effort to get local path
+    val pageId = map[EntryFields.PAGE_ID] as Int? // null means the main entry
+    val mimeType = map[EntryFields.MIME_TYPE] as String
+    val width = map[EntryFields.WIDTH] as Int
+    val height = map[EntryFields.HEIGHT] as Int
+    val rotationDegrees = map[EntryFields.ROTATION_DEGREES] as Int
+    val isFlipped = map[EntryFields.IS_FLIPPED] as Boolean
+    val sizeBytes = toLong(map[EntryFields.SIZE_BYTES])
+    val trashed = map[EntryFields.TRASHED] as Boolean
+    val trashPath = map[EntryFields.TRASH_PATH] as String?
 
     private val isRotated: Boolean
         get() = rotationDegrees % 180 == 90
---
+++ android/app/src/main/kotlin/deckers/thibault/aves/model/EntryFields.kt
@@ -0,0 +1,25 @@
+package deckers.thibault.aves.model
+
+// entry fields exported and imported from/to the platform side
+object EntryFields {
+    const val ORIGIN = "origin" // int
+    const val URI = "uri" // string
+    const val PATH = "path" // string
+    const val PAGE_ID = "pageId" // int
+    const val SOURCE_MIME_TYPE = "sourceMimeType" // string
+    const val MIME_TYPE = "mimeType" // string
+    const val WIDTH = "width" // int
+    const val HEIGHT = "height" // int
+    const val SOURCE_ROTATION_DEGREES = "sourceRotationDegrees" // int
+    const val ROTATION_DEGREES = "rotationDegrees" // int
+    const val IS_FLIPPED = "isFlipped" // boolean
+    const val SIZE_BYTES = "sizeBytes" // long
+    const val TRASHED = "trashed" // boolean
+    const val TRASH_PATH = "trashPath" // string
+    const val TITLE = "title" // string
+    const val DATE_ADDED_SECS = "dateAddedSecs" // long
+    const val DATE_MODIFIED_SECS = "dateModifiedSecs" // long
+    const val SOURCE_DATE_TAKEN_MILLIS = "sourceDateTakenMillis" // long
+    const val DURATION_MILLIS = "durationMillis" // long
+    const val CONTENT_ID = "contentId" // long
+}
