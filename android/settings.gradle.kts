0a187311bce8a1566029fb7f7449483604eddc33
Thibault Deckers <thibault.deckers@gmail.com>
2025-02-14 20:13:34 +0100

    gitignore updates, plugin ref fix

---
+++ android/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.8.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+include(":app")
+include(":exifinterface")
