410136e08a405ced64da27d5610740237482cb34
Thibault Deckers <thibault.deckers@gmail.com>
2026-07-24 11:59:57 +0200

    downgraded Kotlin as CodeQL currently supports <2.4.10

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 
     // define versions (Gradle version catalog cannot be referenced here)
     id("com.android.application") version "9.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
 }
 
 include(":app")
